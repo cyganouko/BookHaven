0d4b1883491f719ed08ca93b2ffa5ce715eccff0
cyganouko <kegoyacygan@gmail.com>
2026-08-19 20:52:54 +0000

    fix: update MongoDB connection and backend port

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ let dbName = 'bookhaven';
 
 // define a url to connect to the database
 const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
-mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
+mongoose.connect(MONGODB_URI)
 let db = mongoose.connection;
 
 // Check Connection
@@ -41,7 +41,7 @@ app.use(cors());
 app.use('/api/books', bookRoute)
 
 // Define the PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)

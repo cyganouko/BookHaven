9d55eb8905057a8db1199639e6e8b8910b8032c8
cyganouko <kegoyacygan@gmail.com>
2026-08-19 21:35:38 +0000

    fix: configure backend for container environment

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(cors());
 app.use('/api/books', bookRoute)
 
 // Define the PORT
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
